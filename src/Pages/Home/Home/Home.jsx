fc2f74f9a7c7929c564ac65e676ce50394044b9f
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2022-11-15 20:00:02 +0600

    This is the next

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
- import InfoCards from '../InfoCards/InfoCards';
+import InfoCards from '../InfoCards/InfoCards';
+import MakeAppointment from '../MakeAppointment/MakeAppointment';
 import Services from '../services/services';
 import Treatment from '../Treatment/Treatment';
 
@@ -12,7 +13,7 @@ const Home = () => {
             <InfoCards></InfoCards>
             <Services></Services>
             <Treatment></Treatment>
-
+            <MakeAppointment></MakeAppointment>
         </div>
     );
 };
--- src/Pages/Home/Treatment/Treatment.jsx
+++ src/Pages/Home/Treatment/Treatment.jsx
@@ -5,11 +5,11 @@ const Treatment = () => {
     return (
         <div>
             <div className="hero">
-                <div className="lg:p-32 hero-content gap-8 flex-col lg:flex-row">
-                    <img src={treatment} className="lg:w-1/4 rounded-lg shadow-2xl" alt='treatment-img' />
+                <div className="lg:p-20 hero-content gap-5 flex-col lg:flex-row">
+                    <img src={treatment} className="lg:w-1/3 rounded-lg shadow-2xl" alt='treatment-img' />
                     <div className='lg:px-10'>
-                        <h1 className="text-5xl lg:font-bold sm:font-semibold">Exceptional Dental Care, on Your Terms!</h1>
-                        <p className="py-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page.</p>
+                        <h1 className="text-3xl lg:font-bold sm:font-semibold">Exceptional Dental Care, on Your Terms!</h1>
+                        <p className="py-3">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page.</p>
                         <button className="btn btn-primary">Get Started</button>
                     </div>
                 </div>
