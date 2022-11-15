3640890b85f2683149a59679749c0e850e934316
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2022-11-15 19:46:05 +0600

    This is the treatment

--- src/Pages/Home/Treatment/Treatment.jsx
+++ src/Pages/Home/Treatment/Treatment.jsx
@@ -4,12 +4,12 @@ import treatment from '../../../assets/images/treatment.png';
 const Treatment = () => {
     return (
         <div>
-            <div className="hero py-5 px-20">
-                <div className="hero-content flex-col justify-between lg:flex-row">
-                    <img src={treatment} className="lg:w-1/2 sm:w-full h-96  rounded-lg shadow-2xl" alt='treatment' />
-                    <div className='px-16'>
-                        <h1 className="text-5xl font-bold">Exceptional Dental Care, on Your Terms!</h1>
-                        <p className="py-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+            <div className="hero bg-base-200">
+                <div className="lg:px-32 hero-content gap-8 flex-col lg:flex-row">
+                    <img src="https://placeimg.com/260/400/arch" className="lg:w-1/4 rounded-lg shadow-2xl" alt='treatment-img'/>
+                    <div className='lg:px-10'>
+                        <h1 className="text-5xl lg:font-bold sm:font-semibold">Exceptional Dental Care, on Your Terms!</h1>
+                        <p className="py-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page.</p>
                         <button className="btn btn-primary">Get Started</button>
                     </div>
                 </div>
