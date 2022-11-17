e9749f8fe587a462cf5fd1d1c059c4598fdd074b
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2022-11-17 21:27:09 +0600

    This is the next

--- src/Pages/Appointment/AvailabelAppointment/AvailabelAppointment.jsx
+++ src/Pages/Appointment/AvailabelAppointment/AvailabelAppointment.jsx
@@ -8,11 +8,12 @@ import { useQuery } from '@tanstack/react-query';
 
 const AvailableAppointment = ({ selected }) => {
     const [treatment, setTreatment] = useState(null);
-    const date = format(selected, 'PP')
+    const date = format(selected, 'PP');
+
     const { data: appointmentOptions = [] } = useQuery({
-        queryKey: 'appointmentOptions',
+        queryKey: ['appointmentOptions', date],
         queryFn: async () => {
-            const res = await fetch(`http://localhost:5000/appointmentOptions?data=${date}`)
+            const res = await fetch(`http://localhost:5000/appointmentOptions?date=${date}`);
             const data = await res.json();
             return data;
         }
