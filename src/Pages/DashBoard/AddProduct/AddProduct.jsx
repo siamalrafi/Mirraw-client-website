3050fb5896ead0400afc11deca116959014ac63f
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2022-11-26 19:55:53 +0600

    This is the products

--- src/Pages/DashBoard/AddProduct/AddProduct.jsx
+++ src/Pages/DashBoard/AddProduct/AddProduct.jsx
@@ -7,13 +7,18 @@ const AddProduct = () => {
 
 
     const handleAddProducts = (data) => {
-        console.log(data.productname);
-        console.log(data.price);
-        console.log(data.condition);
-        console.log(data.phone);
-        console.log(data.location);
-        console.log(data.productcategory);
-        console.log(data.description);
+        const addProductInfo = {
+            productName: data.productname,
+            price: data.price,
+            condition: data.condition,
+            phone: data.phone,
+            location: data.location,
+            productCategory: data.productcategory,
+            description: data.description,
+            year: data.year,
+        }
+
+
 
 
 
@@ -24,88 +29,84 @@ const AddProduct = () => {
         <div>
             <h1>AddProduct</h1>
             <form onSubmit={handleSubmit(handleAddProducts)}>
-                <div className="form-control w-full max-w-xs">
-                    <label className="label"> <span className="label-text">Product Name</span></label>
-                    <input type="text" {...register("productname", {
-                        required: "Product Name is Required"
-                    })} className="input input-bordered w-full max-w-xs" />
-                    {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
-                </div>
-                <div className="form-control w-full max-w-xs">
-                    <label className="label"> <span className="label-text">Price</span></label>
-                    <input type="number" {...register("price", {
-                        required: true
-                    })} className="input input-bordered w-full max-w-xs" />
-                    {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
-                </div>
 
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">Condition</span>
-                    </label>
-                    <select
-                        {...register("condition", {
+
+                <div className='grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 justify-items-center lg:gap-10 md:gap-6 sm:gap-4'>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Product Name</span></label>
+                        <input type="text" {...register("productname", {
+                            required: "Product Name is Required"
+                        })} className="input input-bordered w-full max-w-xs" />
+                        {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Price</span></label>
+                        <input type="number" {...register("price", {
                             required: true
-                        })}
-                        className="select select-bordered w-full max-w-xs">
-                        <option disabled selected>Excellent</option>
-                        <option>Good</option>
-                        <option>Fair</option>
-                    </select>
-                    {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+                        })} className="input input-bordered w-full max-w-xs" />
+                        {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+                    </div>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Condition</span>
+                        </label>
+                        <select
+                            {...register("condition", {
+                                required: true
+                            })}
+                            className="select select-bordered w-full max-w-xs">
+                            <option disabled selected>Excellent</option>
+                            <option>Good</option>
+                            <option>Fair</option>
+                        </select>
+                        {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+                    </div>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Phone Number</span></label>
+                        <input type="number" {...register("phone", {
+                            required: true
+                        })} className="input input-bordered w-full max-w-xs" />
+                        {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Location</span></label>
+                        <input type="text" {...register("location", {
+                            required: true
+                        })} className="input input-bordered w-full max-w-xs" />
+                        {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Product Category</span></label>
+                        <select
+                            {...register("productcategory", {
+                                required: true
+                            })}
+                            className="select select-bordered w-full max-w-xs">
+                            <option disabled selected>Kids jewellery</option>
+                            <option>Fassion jewellery</option>
+                            <option>Diamond</option>
+                        </select>
+                        {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Description</span></label>
+                        <input type="text" {...register("description", {
+                            required: true
+                        })} className="input input-bordered w-full max-w-xs" />
+                        {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Year of Purchase</span></label>
+                        <input type="number" {...register("year", {
+                            required: true
+                        })} className="input input-bordered w-full max-w-xs" />
+                        {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+                    </div>
                 </div>
 
-                <div className="form-control w-full max-w-xs">
-                    <label className="label"> <span className="label-text">Phone Number</span></label>
-                    <input type="number" {...register("phone", {
-                        required: true
-                    })} className="input input-bordered w-full max-w-xs" />
-                    {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
-                </div>
-                <div className="form-control w-full max-w-xs">
-                    <label className="label"> <span className="label-text">Location</span></label>
-                    <input type="text" {...register("location", {
-                        required: true
-                    })} className="input input-bordered w-full max-w-xs" />
-                    {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
-                </div>
-                <div className="form-control w-full max-w-xs">
-                    <label className="label"> <span className="label-text">Product Category</span></label>
-                    <input type="text" {...register("productcategory", {
-                        required: true
-                    })} className="input input-bordered w-full max-w-xs" />
-                    {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
-                </div>
-
-                <div className="form-control w-full max-w-xs">
-                    <label className="label"> <span className="label-text">Description</span></label>
-                    <input type="text" {...register("description", {
-                        required: true
-                    })} className="input input-bordered w-full max-w-xs" />
-                    {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
-                </div>
-                <div className="form-control w-full max-w-xs">
-                    <label className="label"> <span className="label-text">Year of Purchase</span></label>
-                    <input type="number" {...register("year", {
-                        required: true
-                    })} className="input input-bordered w-full max-w-xs" />
-                    {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
-                </div>
-
-
-
-                <div className="form-control w-full max-w-xs">
-                    <label className="label"> <span className="label-text">Password</span></label>
-                    <input type="password" {...register("password", {
-                        required: "Password is required",
-                        minLength: { value: 6, message: "Password must be 6 characters long" },
-                        pattern: { value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/, message: 'Password must have uppercase, number and special characters' }
-                    })} className="input input-bordered w-full max-w-xs" />
-                    {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
-                </div>
-
-
-                <input className='btn btn-accent w-full mt-4' value="Sign Up" type="submit" />
+                <input className='btn btn-primary w-full mt-4' value="Add Product" type="submit" />
                 {signUpError && <p className='text-red-600'>{signUpError}</p>}
                 {/* <Toaster /> */}
             </form>
