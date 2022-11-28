d55c7e1d977db6e69c9c2764b767d98a0c894a22
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2022-11-28 15:36:24 +0600

    This is the last and final commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './Routes/Routes/Routes';
-import { Toaster } from 'react-hot-toast';
 
 
 function App({ children }) {
   return (
     <div className='max-w-[1440px] mx-auto'>
       <RouterProvider router={router}>
-        {children} 
+        {children}
       </RouterProvider>
     </div>
   );
